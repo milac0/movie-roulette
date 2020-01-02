bd6e0bf9b6f506846403aba266e3af5cee1c927b
unknown <milac0@hotmail.com>
2020-01-02 14:48:05 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const PrettierPlugin = require("prettier-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    devtool: "eval-source-map",
+    devServer: {
+        contentBase: "./dist",
+        historyApiFallback: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new PrettierPlugin(),
+        new Dotenv()
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader"
+            }
+        ]
+    },
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: '/'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    }
+};
