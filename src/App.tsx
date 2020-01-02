f5194306a472cc89f606bbf3d0a8237e3e47540b
unknown <milac0@hotmail.com>
2020-01-02 15:57:55 +0100

    add emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { css } from "emotion";
+import { colors } from "./theme/theme";
 
-const App = () => {
-  return <div>Hello from app</div>;
+const text = css`
+  color: ${colors.primary};
+`;
+
+interface Props {}
+
+const App: React.FC<Props> = () => {
+  return <div className={text}>Hello App!</div>;
 };
 
 export default App;
---
+++ src/theme/theme.ts
@@ -0,0 +1,4 @@
+export const colors = {
+  primary: "#19427A",
+  secondary: "#00D6C6"
+};
