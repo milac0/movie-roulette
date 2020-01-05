d2a76906d1f632071c07497e08524ceeea805c8c
unknown <milac0@hotmail.com>
2020-01-05 15:40:04 +0100

    localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import MovieList from "./pages/MovieList";
 import MovieDetails from "./pages/MovieDetails";
-import UserContextProvider from "./context/UserContext";
+import UserContextProvider, { UserContext } from "./context/UserContext";
 import axios from "axios";
+import moment from "moment";
 
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
 interface Props {}
 
 const App: React.FC<Props> = () => {
+  const { user, setUser } = useContext(UserContext);
+  useEffect(() => {
+    const userLocal = JSON.parse(localStorage.user);
+    if (
+      userLocal &&
+      moment().isBefore(
+        moment(userLocal.expires_at, "YYYY-MM-DD HH:mm:ss").utc()
+      )
+    ) {
+      setUser(userLocal);
+    }
+  }, []);
+
   return (
-    <UserContextProvider>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" component={MovieList} exact />
-          <Route path="/movies/:movieid" component={MovieDetails} />
-        </Switch>
-      </Router>
-    </UserContextProvider>
+    <Router>
+      <Navbar />
+      <button onClick={() => console.log(user)}>check</button>
+      <Switch>
+        <Route path="/" component={MovieList} exact />
+        <Route path="/movies/:movieid" component={MovieDetails} />
+      </Switch>
+    </Router>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
+import UserContextProvider from "./context/UserContext";
 
-render(<App />, document.getElementById("root"));
+const jsx = (
+  <UserContextProvider>
+    <App />
+  </UserContextProvider>
+);
+
+render(jsx, document.getElementById("root"));
