383e2fa49dbe440d4bd17da2114aee68e9bf3bb3
unknown <milac0@hotmail.com>
2020-01-09 21:13:11 +0100

    handle 401 error

--- src/components/CustomRating.tsx
+++ src/components/CustomRating.tsx
@@ -16,30 +16,34 @@ const CustomRating: React.FC<Props> = ({ movieid }) => {
   useEffect(() => {
     (async () => {
       // no other way to fetch guest users ratings
-      const ratedMovies = (
-        await axios.get(
-          `/guest_session/${user.guest_session_id}/rated/movies?api_key=${process.env.API_KEY}&language=en-US&sort_by=created_at.asc`
-        )
-      ).data.results;
-      if (ratedMovies) {
-        const movie = ratedMovies.filter(
-          rated => rated.id.toString() === movieid
-        );
-        movie.length > 0 ? setRate(movie[0].rating) : setRate(null);
+      if (isAuth()) {
+        try {
+          const ratedMovies = (
+            await axios.get(
+              `/guest_session/${user.guest_session_id}/rated/movies?api_key=${process.env.API_KEY}&language=en-US&sort_by=created_at.asc`
+            )
+          ).data.results;
+          if (ratedMovies) {
+            const movie = ratedMovies.filter(
+              rated => rated.id.toString() === movieid
+            );
+            movie.length > 0 ? setRate(movie[0].rating) : setRate(null);
+          }
+        } catch (err) {
+          console.log(err);
+        }
       }
     })();
   }, []);
   const handleRating = async (value: number | null) => {
     if (isAuth()) {
       try {
-        const response = (
-          await axios.post(
-            `/movie/${movieid}/rating?api_key=${process.env.API_KEY}&guest_session_id=${user.guest_session_id}`,
-            { value }
-          )
-        ).data;
+        await axios.post(
+          `/movie/${movieid}/rating?api_key=${process.env.API_KEY}&guest_session_id=${user.guest_session_id}`,
+          { value }
+        );
         setRate(value);
-        setMessage(response.status_message);
+        setMessage(null);
       } catch (err) {
         setRate(null);
         setMessage("Error while submiting rating. Try again later.");
