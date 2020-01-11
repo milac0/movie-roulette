d4495edef5324f25966ae3a98c7b5a4bf8b0df94
unknown <milac0@hotmail.com>
2020-01-11 11:47:43 +0100

    add types

--- src/interface/Movie.ts
+++ src/interface/Movie.ts
@@ -1,16 +1,19 @@
 export default interface Movie {
   adult: false;
-  backdrop_path: string;
+  backdrop_path: string | null;
   genre_ids: Array<number>;
   id: 419704;
   original_language: string;
   original_title: string;
   overview: string;
   popularity: number;
-  poster_path: string;
+  poster_path: string | null;
   release_date: string;
   title: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
+  runtime: number | null;
+  genres: Array<any>;
+  production_companies: Array<any>;
 }
