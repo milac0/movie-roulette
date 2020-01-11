b1269132dbb063e4cd609af8a3c58157a90fba14
unknown <milac0@hotmail.com>
2020-01-11 14:14:41 +0100

    refactor filters render to map over Filters

--- src/components/FiltersBar/FiltersBar.tsx
+++ src/components/FiltersBar/FiltersBar.tsx
@@ -13,38 +13,18 @@ const SearchBar: React.FC<Props> = () => {
     setFilterBy(e.currentTarget.value as Filters);
   };
 
-  const { Popular, NowPlaying, TopRated, Upcoming } = Filters;
-
   return (
     <div className={styles.container}>
-      <button
-        onClick={handleClick}
-        value={Popular}
-        className={filterBy === Popular ? styles.activeFilter : null}
-      >
-        Popular
-      </button>
-      <button
-        onClick={handleClick}
-        value={NowPlaying}
-        className={filterBy === NowPlaying ? styles.activeFilter : null}
-      >
-        Now Playing
-      </button>
-      <button
-        onClick={handleClick}
-        value={TopRated}
-        className={filterBy === TopRated ? styles.activeFilter : null}
-      >
-        Top Rated
-      </button>
-      <button
-        onClick={handleClick}
-        value={Upcoming}
-        className={filterBy === Upcoming ? styles.activeFilter : null}
-      >
-        Upcoming
-      </button>
+      {Object.keys(Filters).map((key, i) => (
+        <button
+          key={i}
+          onClick={handleClick}
+          value={Filters[key]}
+          className={filterBy === Filters[key] ? styles.activeFilter : null}
+        >
+          {key}
+        </button>
+      ))}
     </div>
   );
 };
