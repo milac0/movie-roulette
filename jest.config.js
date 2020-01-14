902847bb5a30a203366911a3107ac108af8a3b55
unknown <milac0@hotmail.com>
2020-01-14 14:31:47 +0100

    setup jest & enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: "((\\.|/*.)(spec))\\.ts?$"
-};
+  testEnvironment: 'jsdom',
+  testRegex: "((\\.|/*.)(spec))\\.tsx?$",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "jest-static-stubs/$1",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+  },
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.ts"
+  ]
+}
---
+++ jest.setup.ts
@@ -0,0 +1,7 @@
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const noop = () => { };
+Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
